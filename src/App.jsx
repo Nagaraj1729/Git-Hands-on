6e8c5ea62e2432ebd3af317a83c7b98cfc246343
Nagaraj1729 <nagaraj91777@gmail.com>
2024-08-19 15:21:15 +0530

    project setup initially

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+   <div>
+      <h1>Vite + React Project</h1>
+      <p>This is created to practice GitHub version control</p>
+   </div>
+  )
+}
+
+export default App
