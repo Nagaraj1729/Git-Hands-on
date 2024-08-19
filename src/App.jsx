c04e43316f93cdc35405fd8822bf5ff2f34b15df
Nagaraj1729 <nagaraj91777@gmail.com>
2024-08-19 15:36:52 +0530

    sample styles added

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import './App.css'
 function App() {
 
   return (
-   <div>
-      <h1>Vite + React Project</h1>
-      <p>This is created to practice GitHub version control</p>
+   <div className='bg-gray-900/50 h-screen text-center py-24'>
+      <h1 className='text-3xl font-semibold mb-2'>Vite + React Project</h1>
+      <p className='text-lg'>This is created to practice GitHub version control</p>
    </div>
   )
 }
