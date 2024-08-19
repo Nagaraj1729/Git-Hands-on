274d170d1785cfe7eeac934505659eb7d512466c
Nagaraj1729 <nagaraj91777@gmail.com>
2024-08-19 16:22:56 +0530

    header added

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ function App() {
 
   return (
    <div className='bg-gray-900/50 h-screen text-center py-24'>
+    <header className='bg-gray-300 py-4 fixed w-full top-0 font-medium tracking-widest'> Sample Header</header>
       <h1 className='text-3xl font-semibold mb-2'>Vite + React Project</h1>
       <p className='text-lg'>This is created to practice GitHub version control</p>
       <Card/>
