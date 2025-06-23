4438a998d823b7ffe8e300d1c967d47c3f2eeedc
Girish0201 <bathigirish@gmail.com>
2025-06-23 12:27:50 +0530

    add heading

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,27 @@
-import './App.css'
-import Card from './components/Card'
-import Address from './components/Address'
+import "./App.css";
+import Card from "./components/Card";
+import Address from "./components/Address";
 
 function App() {
-
   return (
-   <div className='bg-gray-900/50 h-screen text-center py-24'>
-    <header className='bg-gray-300 py-4 fixed w-full top-0 font-medium tracking-widest'> Sample Header</header>
-      <h1 className='text-3xl font-semibold mb-2'>Vite + React Project</h1>
-      <p className='text-lg'>This is created to practice GitHub version control</p>
-<p>learning git hub repos how to push and pull request </p>
-     <p>Personal Details para line</p>
-      <Card/>
-     <Address/>
-     <h1>My First Heading</h1>
-     <h2>Second Heading</h2>
-   </div>
-  )
+    <div className="bg-gray-900/50 h-screen text-center py-24">
+      <header className="bg-gray-300 py-4 fixed w-full top-0 font-medium tracking-widest">
+        {" "}
+        Sample Header
+      </header>
+      <h1 className="text-3xl font-semibold mb-2">Vite + React Project</h1>
+      <p className="text-lg">
+        This is created to practice GitHub version control
+      </p>
+      <p>learning git hub repos how to push and pull request </p>
+      <p>Personal Details para line</p>
+      <h3>third heading</h3>
+      <Card />
+      <Address />
+      <h1>My First Heading</h1>
+      <h2>Second Heading</h2>
+    </div>
+  );
 }
 
-export default App
+export default App;
